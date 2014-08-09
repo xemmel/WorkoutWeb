71b18660e9f9bb0101d2a18671ad05176ca1d6bf
xemmel <lacour@gmail.com>
2014-08-09 12:33:19 +0200

    New GraphSetup

--- WorkoutWeb/App_Start/BundleConfig.cs
+++ WorkoutWeb/App_Start/BundleConfig.cs
@@ -10,6 +10,8 @@ namespace WorkoutWeb
     {
       bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                   "~/Scripts/jquery-{version}.js"));
+      bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                "~/Scripts/angular.js"));
       bundles.Add(new ScriptBundle("~/bundles/bootjs").Include(
                 "~/Scripts/bootstrap.js"));
 
--- WorkoutWeb/Controllers/GraphController.cs
+++ WorkoutWeb/Controllers/GraphController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,11 +18,38 @@ namespace WorkoutWeb.Controllers
     {
       return View();
     }
+    
     public ActionResult ShowGraph()
     {
       return View();
     }
 
+    [HttpPost]
+    public ActionResult PostPieData(PieValue p)
+    {
+      db.PieValues.Add(p);
+      db.SaveChanges();
+      return Json(p, JsonRequestBehavior.AllowGet);
+    }
+
+    [HttpDelete]
+    public ActionResult DelPie(int id)
+    {
+      PieValue p = db.PieValues.Find(id);
+      db.PieValues.Remove(p);
+      db.SaveChanges(); 
+
+      return Json(p);
+    }
+
+    [HttpPut]
+    public ActionResult UpdatePie(int id, PieValue p)
+    {
+      db.Entry(p).State = EntityState.Modified;
+      db.SaveChanges();
+      return Json(p);
+    }
+    
     public ActionResult GetPieData()
     {
       List<PieValue> data = db.PieValues.ToList();
